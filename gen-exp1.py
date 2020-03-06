e81288f95801959f7012c9c5fc94b1b25e777088
LaGvidilo <cyrilcoelho67@gmail.com>
2020-03-06 03:02:39 +0100

    no coit

--- gen-exp1.py
+++ gen-exp1.py
@@ -5,15 +5,27 @@ from PIL import Image
 
 unidivfact = GP.GP_SymReg(500,100,0.01)
 unidivfact.load("uni-divfact-D.model")
-sizeworld = (1024,1024)
+posflatconv = GP.GP_SymReg(500,100,0.01)
+posflatconv.load("pos-flat-convertor.model")
+
+
+sizeworld = (512,512)
+szd = int((sizeworld[0] * sizeworld[1])/2)
+
 
 image = Image.new("RGB", sizeworld)
-image.putpixel((x,y),(color))
-image.save(str(i)+".png", "PNG")
 
+for i in range(1,255):
+	for x in range(1,sizeworld[0]):
+		for y in range(1,sizeworld[1]):
+			print((i/255.0)*100.0,"% - ",(x/sizeworld[0])*100.0,"% - ",(y/sizeworld[1])*100.0,"%")
+			z2 = [x,y,szd]
+			z2 = list(map(float, z2))
+			y2 = int(posflatconv.predict(z2))
 
-z = map(float, z)
-int(unidivfact.predict(z))
+			z1 = [i,y2]
+			z1 = list(map(float, z1))
+			y1 = int(unidivfact.predict(z1))
+			if y1 > 0:image.putpixel((x,y),(y1*i,y1*i,y1*i))
 
-
-for x1 in range(1,sizeworld[0]):
+image.save("g-exp1.png", "PNG")
