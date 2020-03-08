c33020fddd85ff4963246e6995285d2114309551
LaGvidilo <cyrilcoelho67@gmail.com>
2020-03-08 05:11:07 +0100

    maj

--- gen-exp3.py
+++ gen-exp3.py
@@ -1,7 +1,8 @@
-#coding: utf-8
+
 
 import gplearn.GP as GP
 from PIL import Image 
+from multiprocessing import Pool
 
 unidivfact = GP.GP_SymReg(500,100,0.01)
 unidivfact.load("uni-divfact-D.model")
@@ -9,28 +10,35 @@ posflatconv = GP.GP_SymReg(500,100,0.01)
 posflatconv.load("pos-flat-convertor.model")
 
 
-sizeworld = (512,512)
+sizeworld = (1024,1024)
 szd = int((sizeworld[0] * sizeworld[1])/2)
 
 
 image = Image.new("RGB", sizeworld)
 
-for i in range(1,255):
+
+def func1(i):
 	for x in range(1,sizeworld[0]):
 		for y in range(1,sizeworld[1]):
 			print((i/255.0)*100.0,"% - ",(x/sizeworld[0])*100.0,"% - ",(y/sizeworld[1])*100.0,"%")
 			z2 = [x,y,i]
 			z2 = list(map(float, z2))
 			y2 = int(posflatconv.predict(z2))
-			"""
-			z1 = [i,y2]
+			
+			z1 = [i%16,y2]
 			z1 = list(map(float, z1))
 			y1 = int(unidivfact.predict(z1))
-			print(y2,y1)
 
 			if y1 != 0:
-				image.putpixel((x,y),(i,i,i))
-			"""
-			image.putpixel((x,y),((i*y2)%256,(i*y2)%256,(i*y2)%256))
+				image.putpixel((x,y),((i*y2)%256,((i%128)*y2)%i,((i%8)*y2)%128))
+	return str(i)+" Done."
+			
 
-image.save("g-exp1.png", "PNG")
+
+with Pool(10) as p:
+	print(p.map(func1, range(1,255)))
+
+
+
+
+image.save("g-exp4.png", "PNG")
